a9d900eb76d392a6f143cf22469fcc9ad49d38e4
tarik setia <setia.tarik@gmail.com>
2023-05-16 22:59:54 +0530

    Movielens: added Movielense Hook

---
+++ dags/custom/hooks.py
@@ -0,0 +1,62 @@
+import os
+import requests
+from typing import Any
+from airflow.hooks.base import BaseHook
+
+MOVIESLENS_HOST = os.environ.get("MOVIESLENS_HOST", "movielens")
+MOVIESLENS_SCHEMA = os.environ.get("MOVIESLENS_SCHEMA", "http")
+MOVIESLENS_PORT = os.environ.get("MOVIESLENS_PORT", "5000")
+MOVIESLENS_USER = os.environ["MOVIESLENS_USER"]
+MOVIESLENS_PASSWORD = os.environ["MOVIESLENS_PASSWORD"]
+
+
+class MovielensHook(BaseHook):
+    DEFAULT_HOST = "movielens"
+    DEFAULT_SCHEMA = "http"
+    DEFAULT_PORT = 5000
+
+    def __init__(self, conn_id):
+        super().__init__()
+        self._conn_id = conn_id
+        self._session = None
+        self._base_url = None
+
+    def get_conn(self) -> Any:
+        if self._session is None:
+            config = self.get_connection(self._conn_id)
+
+            schema = config.schema or self.DEFAULT_SCHEMA
+            host = config.host or self.DEFAULT_HOST
+            port = config.port or self.DEFAULT_PORT
+
+            self._base_url = f"{schema}://{host}:{port}"
+
+            self._session = requests.Session()
+            if config.login:
+                self._session.auth = (config.login, config.password)
+
+        return self._session, self._base_url
+
+    def get_ratings(self, start_date=None, end_date=None, batch_size=100):
+        yield from self._get_with_pagination(
+            endpoint="/ratings",
+            params={"start_date": start_date, "end_date": end_date},
+            batch_size=batch_size,
+        )
+
+    def _get_with_pagination(self, endpoint, params, batch_size=100):
+        session, base_url = self.get_conn()
+        offset = 0
+        total = None
+        while total is None or offset < total:
+            response = session.get(
+                f"{base_url}/{endpoint}",
+                params={**params, **{"offset": offset, "limit": batch_size}},
+            )
+            response.raise_for_status()
+            response_json = response.json()
+
+            yield from response_json["result"]
+
+            offset += batch_size
+            total = response_json["total"]
--- dags/movielens.py
+++ dags/movielens.py
@@ -7,7 +7,7 @@ import pandas as pd
 from airflow import DAG
 from airflow.utils import dates
 from airflow.operators.python import PythonOperator
-
+from custom.hooks import MovielensHook
 
 MOVIESLENS_HOST = os.environ.get("MOVIESLENS_HOST", "movielens")
 MOVIESLENS_SCHEMA = os.environ.get("MOVIESLENS_SCHEMA", "http")
@@ -16,49 +16,8 @@ MOVIESLENS_USER = os.environ["MOVIESLENS_USER"]
 MOVIESLENS_PASSWORD = os.environ["MOVIESLENS_PASSWORD"]
 
 
-def _get_session():
-    """Builds a request session for MovielensAPI"""
-    session = requests.Session()
-    session.auth = (MOVIESLENS_USER, MOVIESLENS_PASSWORD)
-    base_url = f"{MOVIESLENS_SCHEMA}://{MOVIESLENS_HOST}:{MOVIESLENS_PORT}"
-    return session, base_url
 
-
-def _get_with_pagination(session, url, params, batch_size=100):
-    """Fetches records using GET request with given URL/params
-    Taking pagination into account
-    """
-    logger = logging.getLogger(__name__)
-    offset = 0
-    total = None
-    while total is None or offset < total:
-        response = session.get(
-            url, params={**params, **{"offset": offset, "limit": batch_size}}
-        )
-        logger.info(f"URL={response.url}")
-        response.raise_for_status()
-        response_json = response.json()
-
-        yield from response_json["result"]
-
-        offset += batch_size
-        total = response_json["total"]
-
-
-def _get_ratings(start_date, end_date, batch_size=100):
-    logger = logging.getLogger(__name__)
-    
-    session, base_url = _get_session()
-    logger.info(base_url)
-    yield from _get_with_pagination(
-        session=session,
-        url=base_url + "/ratings",
-        params={"start_date": start_date, "end_date": end_date},
-        batch_size=batch_size,
-    )
-
-
-def _fetch_ratings(templates_dict, batch_size=1000):
+def _fetch_ratings(conn_id, templates_dict, batch_size=1000):
     logger = logging.getLogger(__name__)
 
     start_date = templates_dict["start_date"]
@@ -66,8 +25,9 @@ def _fetch_ratings(templates_dict, batch_size=1000):
     output_path = templates_dict["output_path"]
 
     logger.info(f"Fetching ratings from {start_date} to {end_date}")
+    hook = MovielensHook(conn_id=conn_id)
     ratings = list(
-        _get_ratings(start_date=start_date, end_date=end_date, batch_size=batch_size)
+        hook.get_ratings(start_date=start_date, end_date=end_date, batch_size=batch_size)
     )
     logger.info(f"Fetched {len(ratings)} ratings.")
     logger.info(f"Writing ratings to {output_path}")
@@ -111,6 +71,9 @@ with DAG(
     fetch_ratings = PythonOperator(
         task_id="fetch_rating",
         python_callable=_fetch_ratings,
+        op_kwargs={
+            "conn_id": "movielens"
+        },
         templates_dict={
             "start_date": "{{ds}}",
             "end_date": "{{next_ds}}",
